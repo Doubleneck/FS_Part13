4db765c1be556a0fb72aa98b0175c126ac8a5475
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2024-01-09 12:43:25 +0200

    exercise 13.5

---
+++ blog_backend/controllers/blogs.js
@@ -0,0 +1,35 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+    const blogs = await Blog.findAll()
+    res.json(blogs)
+  })
+  
+router.post('/', async (req, res) => {
+  const blog = await Blog.create(req.body)
+  res.json(blog)
+})
+
+const blogFinder = async (req, res, next) => {
+    req.blog = await Blog.findByPk(req.params.id)
+    next()
+  } 
+
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+  
+router.delete('/:id', blogFinder, async (req, res) => {  
+  if (req.blog) {
+    await req.blog.destroy()
+  } 
+  res.status(204).end()
+})
+
+module.exports = router
